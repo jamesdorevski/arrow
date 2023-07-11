733a1c1662a444ede03492fd94e78a08b03f871a
James Dorevski <github@jamesdorevski.com>
2023-07-11 16:33:51 +1000

    clap boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Command, Arg, ArgAction};
+
 fn main() {
     println!("Hello, world!");
+
+    let matches = Command::new("Arrow")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("project")
+                .about("Add, view or remove your projects")
+                .arg(
+                    Arg::new("list")
+                        .short('l')
+                        .long("list")
+                        .action(ArgAction::SetTrue)
+                        .help("list all created projects")
+                )
+                .arg(
+                    Arg::new("add")
+                        .short('a')
+                        .long("add")
+                )
+        )
+        .get_matches();
+    
+    match matches.subcommand() {
+        Some(("project", sub_matches)) => {
+            println!("project list used.");
+            
+            let list_selected = sub_matches.get_flag("list");
+            println!("{}", list_selected);
+        },
+        _ => unreachable!("Exhausted list of subcommands and "),
+    }
 }
