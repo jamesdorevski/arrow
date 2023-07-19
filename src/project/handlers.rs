379a1927d9a7de22dea96538a259e7f5cbb5a51f
James Dorevski <github@jamesdorevski.com>
2023-07-19 11:42:03 +1000

    refactored project handlers

--- src/project/handlers.rs
+++ src/project/handlers.rs
@@ -1,19 +1,26 @@
 use chrono::Local;
 
 use crate::model::Project;
-use crate::project::repository;
 
+use super::repository::Repository;
 use super::print::TablePadding;
+
+fn repo_conn() -> Repository {
+    Repository::new().expect("Failed to connect to repository!")
+}
 
 pub fn add(name: &str) {
     let mut new_proj = Project::new(0, name.to_string(), Local::now(), Local::now());
-    new_proj.id = repository::save_project(&new_proj);
+    let repo = repo_conn();
+
+    new_proj.id = repo.save_proj(&new_proj).expect("Failed to create new project!");
 
     println!("Created project {}", new_proj);
 }
 
 pub fn list() {
-    let projs = repository::get_projects().expect("Error retrieving projects");
+    let repo = repo_conn();
+    let projs = repo.all_projects().expect("Error retrieving your projects");
 
     let names: Vec<String> = projs.iter().map(|proj| proj.name.to_string()).collect();
     let max_name_len = max_str_len(names);
@@ -23,13 +30,16 @@ pub fn list() {
 }
 
 pub fn remove(id: &i64) {
-    repository::remove_proj(id);
+    let repo = repo_conn();
+    repo.remove_proj(id);
 }
 
 pub fn get(id: &i64) {
+    let conn = repo_conn();
+
     // TODO: use anyway library to improve error msgs
-    let proj = repository::get_project(id).expect("Error retrieving proj");
-    let logs = repository::get_project_logs(id).expect("Error retrieving logs");
+    let proj = conn.get_project(id).expect("Error retrieving project!");
+    let logs = conn.get_project_logs(id).expect("Error retrieving logs!");
 
     println!("{}", proj);
 
