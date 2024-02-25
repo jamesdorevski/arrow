4e8d46ae4cb1dc41edc7cd35320e95727c3cf08d
James Dorevski <github@jamesdorevski.com>
2024-02-26 10:03:17 +1100

    feat: log start handler complete

--- src/log/handlers.rs
+++ src/log/handlers.rs
@@ -8,40 +8,40 @@ fn repo_conn() -> impl Repository {
     Sqlite::new().expect("Failed to connect to repository!")
 }
 
-pub fn start_logging(proj: String, msg: Option<String>) {
-    todo!();
-    // let interrupted = Arc::new(AtomicBool::new(false));
-    // let interrupted_clone = interrupted.clone();
-    // let repo = repo_conn();
+pub fn start_logging(proj: String, msg: String) {
+    let interrupted = Arc::new(AtomicBool::new(false));
+    let interrupted_clone = interrupted.clone();
+    let repo = repo_conn();
 
-    // let proj_id = match repo.get_project(&proj) {
-    //     Ok(id) => id,
-    //     Err(e) => {
-    //         eprintln!("Error retrieving project: {}", e);
-    //         return None;
-    //     }
-    // };
+    let project_id = match repo.get_project_by_name(&proj) {
+        Ok(p) => p.id,
+        Err(e) => {
+            eprintln!("Error retrieving project: {}", e);
+            return;
+        }
+    }
 
-    // ctrlc::set_handler(move || {
-    //     println!("received Ctrl+C!");
-    //     interrupted_clone.store(true, Ordering::SeqCst);
-    // })
-    // .expect("Error setting Ctrl+C handler");
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+        interrupted_clone.store(true, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl+C handler");
 
-    // let start = Local::now();
-    // println!("Started log at {}", start);
+    let start = Local::now();
+    println!("Started log at {}", start);
 
-    // // hold thread until ctrl c is pressed
-    // while !interrupted.load(Ordering::SeqCst) {}
+    // hold thread until ctrl c is pressed
+    while !interrupted.load(Ordering::SeqCst) {}
 
-    // let end = Local::now();
-    // println!("Finished log at {}", end);
+    let end = Local::now();
+    println!("Finished log at {}", end);
 
-    // let mut log = Log::new(0, *proj_id, msg, start, end);
-    // log.id = repo.save_log(&log).expect("Failed to save log!");
+    let mut log = Log::new(0, project_id, msg, start, end);
 
-    // println!("Created log {}", log.message);
-    // Some(log)
+    match repo.save_log(&project_id, &log) {
+        Ok(_) => println!("Created log {}", log.message),
+        Err(e) => eprintln!("Failed to save log: {}", e),
+    }
 }
 
 // pub fn save_log(proj_id: &u32, msg: Option<String>, dur: &u16) -> Option<Log> {
